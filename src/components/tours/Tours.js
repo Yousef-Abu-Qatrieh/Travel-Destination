f3a5edc93d15c2f5f0064115f2494eba80d24c49
yousef Abu Qatrieh <yousef_1995q@hotmail.com>
2022-02-14 21:24:45 +0200

    l

--- src/components/tours/Tours.js
+++ src/components/tours/Tours.js
@@ -3,9 +3,9 @@ function Tours(props) {
 
     return (
         <>
-            {props.data.map(meme => {
+            {props.data.map((meme,idx) => {
                 return (
-                    <div className='body'>
+                    <div key={idx} className='body'>
                         <h3>Name : {meme.name}</h3>
                         <img src={meme.image} alt={meme.name} />
                         
